398faa9bf2163cc49b74e62cba054b63b7b44086
jade decordova <lacroixjaded@gmail.com>
2020-07-19 01:35:41 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,8 +18,8 @@ end
 
 def output_array(array)
     counter = 0
-   until counter == array.length-1
-          puts  "   " + array[counter]
+    until counter == array.length-1
+          puts array[counter]
         counter += 1
    end
 end
